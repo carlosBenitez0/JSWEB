3ea568991156a5483e80e72c90a70982b492e126
Carlos Benítez <cb2724136@gmail.com>
2024-10-15 21:51:11 -0600

    feat: add excercises 4-5

--- primerosPasos/js/script.js
+++ primerosPasos/js/script.js
@@ -111,9 +111,9 @@ console.log(mensaje); */
 
 //-Verificar si la persona tiene el dinero suficiente para comprar la computadora
 //-Si tiene el dinero suficiente, tener una variable boolenana que diga si la persona puede comprar la computadora o no (true o false)
-
+//////////////////////////////////////////////////////////////////////////////////
 //-Utilizar un operador ternario para asignarle un mensaje a una variable en base al valor de la variable boleana
-const input = document.getElementById("dineroDisponible");
+/* const input = document.getElementById("dineroDisponible");
 const boton = document.getElementById("verificar");
 const precio = document.getElementById("precio");
 const precioComputadora = parseFloat(precio.innerText);
@@ -146,8 +146,116 @@ input.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     codigoEventos();
   }
+}); */
+//////////////////////////////////////////////////////////////////////////////////
+/* const cantidad = document.getElementById("cantidad"); //<p></p>
+const btn_aumentar = document.getElementById("btn-aumentar"); //<button></button>
+const btn_disminuir = document.getElementById("btn-disminuir"); //<button></button>
+const btn_ce = document.getElementById("btn-ce"); //<button></button>
+
+btn_aumentar.addEventListener("click", () => {
+  if (parseInt(cantidad.innerText) < 10) {
+    //0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    cantidad.innerText = parseInt(cantidad.innerText) + 1;
+  }
 });
 
+btn_disminuir.addEventListener("click", () => {
+  if (parseInt(cantidad.innerText) > 0) {
+    cantidad.innerText = parseInt(cantidad.innerText) - 1;
+  }
+});
+
+btn_ce.addEventListener("click", () => {
+  cantidad.innerText = 0;
+}); */
+//////////////////////////////////////////////////////////////////////////////////
+
+//ejercicio 5
+//obtener el valor de los inputs y mandarlos por parametros a la funcion "authentication"
+//verificar si los 2 passwords son iguales y los 2 nombres tambien son iguales
+//si son iguales retornar true, sino, false
+//En base al retorno, mostrar welcome si es true o error si es false
+
+//db
+/* const userDB = "admin";
+const passwordDB = "123";
+
+const message = document.getElementById("message"); //<h1></h1>
+const input_name = document.getElementById("name"); //<input></input>
+const input_password = document.getElementById("password"); //<input></input>
+const btn_login = document.getElementById("btn-login"); //<button></button>
+
+const authentication = (nameUser, passwordUser) => {
+  if (nameUser == userDB && passwordUser == passwordDB) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//validaciones para el usuario
+//El nombre de usuario que no lleve caracteres especiales
+//El nombre de usuario que no lleve numeros
+//El nombre de usuario debe tener al menos 3 caracteres
+//El nombre de usuario debe tener maximo 10 caracteres
+
+//validaciones para la contraseña
+//Que tenga al menos 1 carcater especial
+//Que tenga al menos 1 numero
+//El nombre de usuario debe tener al menos 3 caracteres
+//El nombre de usuario debe tener maximo 10 caracteres
+
+const showMessage = (login) => {
+  if (login) {
+    message.innerText = "Welcome";
+  } else {
+    message.innerText = "Error";
+  }
+};
+
+btn_login.addEventListener("click", () => {
+  const nameUser = input_name.value;
+  const passwordUser = input_password.value;
+
+  const login = authentication(nameUser, passwordUser);
+  showMessage(login);
+}); */
+
+//////////////////////////////////////////////////////////////////////////////////
+//ejercicio 5.2
+const userDB = "admin";
+const passwordDB = "123";
+
+const message = document.getElementById("message"); //<h1></h1>
+const input_name = document.getElementById("name"); //<input></input>
+const input_password = document.getElementById("password"); //<input></input>
+const btn_login = document.getElementById("btn-login"); //<button></button>
+
+const authenticationUser = (nameUser) => {
+  //validaciones para el usuario
+  //El nombre de usuario que no lleve caracteres especiales
+  //El nombre de usuario que no lleve numeros
+  //El nombre de usuario debe tener al menos 3 caracteres
+  //El nombre de usuario debe tener maximo 10 caracteres
+  //usar for
+  //validaciones para la contraseña
+  //Que tenga al menos 1 carcater especial
+  //Que tenga al menos 1 numero
+  //El nombre de usuario debe tener al menos 3 caracteres
+  //El nombre de usuario debe tener maximo 10 caracteres
+};
+
+input_name.addEventListener("input", () => {
+  const nameUser = input_name.value;
+  authenticationUser(nameUser);
+  // const passwordUser = input_password.value;
+
+  // const login = authentication(nameUser, passwordUser);
+  // message.innerText = login;
+});
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
 // variables y constantes ✅
 ///////////////////////////////////////
@@ -163,7 +271,7 @@ input.addEventListener("keydown", (event) => {
 ///////////////////////////////////////
 // function✅
 ///////////////////////////////////////
-// return
+// return✅
 ///////////////////////////////////////
 // default
 ///////////////////////////////////////
